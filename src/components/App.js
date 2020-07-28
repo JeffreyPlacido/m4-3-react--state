43dc28e0b67a235c3722db1fe95184954898d70d
Jeffrey <jeffreyplacido@hotmail.com>
2020-07-28 01:17:36 -0400

    E1-2

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from "./GlobalStyles";
+import data from "../data";
+import Typeahead from "./Typeahead";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 50%;
+  margin: 30px auto;
+  vertical-align: center;
+  ul {
+    padding: 5px;
+    border-radius: 4px;
+    box-shadow: 4px 4px 9px 4px rgba(0, 0, 0, 0.75);
+  }
+`;
+
+const BoxBox = styled.div`
+  position: absolute;
+  top: 50%;
+`;
 
 const App = (props) => {
   return (
     <>
       <GlobalStyles />
-      {/* TODO */}
+      <Wrapper>
+        <BoxBox>
+          <Typeahead
+            suggestions={data.books}
+            handleSelect={(suggestion) => {
+              window.alert(suggestion);
+            }}
+          />
+        </BoxBox>
+      </Wrapper>
     </>
   );
 };
---
+++ src/components/Typeahead.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: white;
+  background-color: blue;
+  padding: 8px;
+  border-radius: 4px;
+`;
+const Input = styled.input`
+  border-radius: 4px;
+  padding: 8px;
+  border: 1px solid grey;
+  margin: 5px;
+`;
+
+const Suggestion = styled.li`
+  padding: 4px;
+  &:hover {
+    background-color: lightsalmon;
+  }
+`;
+
+const Typeahead = ({ suggestions, handleSelect }) => {
+  const [search, setSearch] = React.useState("");
+  if (search.length > 1) {
+    return (
+      <>
+        <Input
+          type="text"
+          name="searchBox"
+          value={search}
+          onChange={(ev) => {
+            setSearch(ev.target.value);
+          }}
+          onKeyDown={(ev) => {
+            if (ev.key === "Enter") {
+              return search;
+            }
+          }}
+        />
+        <Button onClick={() => setSearch("")}>Clear</Button>
+        <ul>
+          {suggestions
+            .filter((obj) => {
+              return obj.title.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((suggestion) => {
+              return (
+                <Suggestion
+                  key={suggestion.id}
+                  onClick={() => handleSelect(suggestion.title)}
+                >
+                  {suggestion.title}
+                </Suggestion>
+              );
+            })}
+        </ul>
+      </>
+    );
+  }
+  return (
+    <>
+      <Input
+        type="text"
+        name="searchBox"
+        value={search}
+        onChange={(ev) => {
+          setSearch(ev.target.value);
+        }}
+        onKeyDown={(ev) => {
+          if (ev.key === "Enter") {
+            return search;
+          }
+        }}
+      />
+      <Button onClick={() => setSearch("")}>Clear</Button>
+    </>
+  );
+};
+
+export default Typeahead;
